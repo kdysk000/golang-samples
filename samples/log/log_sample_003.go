00a81595b478017ce7a5590b8b0d59880e0a0188
kudo_y <sample@japan.com>
2023-11-04 21:26:28 +0900

    add samples

--- samples/log/log_sample_003.go
+++ samples/log/log_sample_003.go
@@ -11,14 +11,14 @@ import (
 	func SetFlags(flag int)
 	  param:
 	    flag: ログのフォーマット用定数。論理和で複数組み合わせ可能
-		      デフォルトは LstdFlags
+	          デフォルトは LstdFlags
 	      Ldate         = 1 << iota
-          Ltime
-          Lmicroseconds
-          Llongfile
-          Lshortfile
-          LUTC
-          LstdFlags     = Ldate | Ltime
+	      Ltime
+	      Lmicroseconds
+	      Llongfile
+	      Lshortfile
+	      LUTC
+	      LstdFlags     = Ldate | Ltime
 	  return:
 	    なし
 */
